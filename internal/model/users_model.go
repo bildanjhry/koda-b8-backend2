b7fb4e0e6012b7dce2f7f3370407bbb5c26aadd5
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-22 14:37:05 +0700

    feat: add users model for upload picture

--- internal/model/users_model.go
+++ internal/model/users_model.go
@@ -6,6 +6,7 @@ type Users struct {
 	Id        int64     `json:"id"`
 	Email     string    `json:"email"`
 	Password  string    `json:"-"`
+	Picture   *string   `json:"picture"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"-"`
 }
