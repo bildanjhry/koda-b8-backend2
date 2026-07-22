2edb698525478882f0c9c91f6a13ae10d064d5e7
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-22 21:28:38 +0700

    feat: add more endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,20 @@ import (
 	"github.com/bildanjhry/auth/internal/middleware"
 	"github.com/bildanjhry/go_shared-lib/utils"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+//	@title			Backend Auth
+//	@version		1.0
+//	@description	This is a sample server celler server.
 
 func main() {
 	r := gin.Default()
 	c := di.NewContainer()
 	userHandler := c.UserHandler()
 	utils.LoadEnv()
+	r.Static("/uploads", "./uploads")
 
 	auth := r.Group("/auth")
 	auth.Use(middleware.Cors())
@@ -22,6 +29,7 @@ func main() {
 
 	users := r.Group("/user")
 	users.Use(middleware.Cors())
+	users.Use(middleware.Auth())
 	users.OPTIONS("/all", func(ctx *gin.Context) {})
 	users.GET("/all", userHandler.GetAll)
 	users.OPTIONS("/delete/:id", func(ctx *gin.Context) {})
@@ -30,8 +38,9 @@ func main() {
 	users.PATCH("/edit/:id", userHandler.GetById)
 	users.OPTIONS("/detail/:id", func(ctx *gin.Context) {})
 	users.GET("/detail/:id", userHandler.GetById)
-	users.OPTIONS("/detail/:id", func(ctx *gin.Context) {})
-	users.GET("/detail/:id", userHandler.GetById)
+	users.OPTIONS("/upload-pic/:id", func(ctx *gin.Context) {})
+	users.PATCH("/upload-pic/:id", userHandler.UploadPicture)
 
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run("0.0.0.0:8080")
 }
